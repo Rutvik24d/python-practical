5dfa8de29d5d120a38c0d97cec09733c93eec5ef
Rutvik coding <143089043+Rutvik24d@users.noreply.github.com>
2023-08-25 16:40:47 +0530

    Add files via upload

---
+++ p-paython/p1.py
@@ -0,0 +1,16 @@
+# two number print to sum
+
+'''     a = 80
+        b = 40
+        sum = a+b
+
+        print(sum)'''
+
+
+# user to input get value
+
+num1 = input("Enter first number : ")
+num2 = input("Enter second number : ")
+
+sum = int(num1) + int(num2)
+print("sum = ",sum)
